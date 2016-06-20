c8015373858f4a990b5bfdb1e84ae95c8147675c
Dominic Althaus <althaus.dominic@gmail.com>
2016-06-20 17:51:09 +0200

    should work somewhat

--- lib/polymer-rails/processors/component.rb
+++ lib/polymer-rails/processors/component.rb
@@ -3,21 +3,7 @@ module Polymer
     module Processors
       class Component
         def self.call(input)
-          byebug
-          input[:data] = `vulcanize --inline-scrips --inline-css --strip-comments #{calculate_redirects} "#{input[:filename]}"`
-        end
-
-        private
-        def self.calculate_redirects
-          return '' if ::Rails.application.assets.paths.count <= 1
-
-          default_asset_path = ::Rails.root.join 'app', 'assets', 'components'
-
-          redirects = ::Rails.application.assets.paths.map do |p|
-            "\"#{default_asset_path}|#{p}\""
-          end
-
-          "--redirect #{redirects.join ' --redirect'}"
+          input[:data] = `vulcanize --inline-scrips --inline-css --strip-comments "#{input[:filename]}"`
         end
       end
     end
