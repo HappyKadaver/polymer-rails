7d1d4efe07b459e4e547e6f642ea4a1c72de65bc
Dominic Althaus <althaus.dominic@gmail.com>
2016-06-20 15:30:04 +0200

    progress

--- lib/polymer-rails/processors/component.rb
+++ lib/polymer-rails/processors/component.rb
@@ -1,35 +1,30 @@
-require "polymer-rails/processors/sprockets_processor"
 require 'open3'
 
 module Polymer
   module Rails
     module Processors
       class Component
-        DEFAULT_ASSET_PATH = Rails.root.join 'app', 'assets', 'components'
-
-        def initialize(data)
-          @data = data
-        end
-
-        def process
+        def self.call(input)
           result = ''
 
           cmd = "vulcanize --inline-scrips --inline-css --strip-comments \"#{calculate_redirects}\""
           Open3.popen3(cmd) do |stdin, stdout, stderr, wait_thr|
-            stdin.write @data
+            stdin.write input[:data]
 
             result = stdout.read
           end
 
-          return result
+          input.merge(data: result)
         end
 
         private
-        def calculate_redirects
-          return '' if Rails.app.assets.paths.count <= 1
+        def self.calculate_redirects
+          return '' if ::Rails.application.assets.paths.count <= 1
 
-          redirects = Rails.app.assets.paths.map do |p|
-            "\"#{DEFAULT_ASSET_PATH}|#{File.join p, 'components'}\""
+          default_asset_path = ::Rails.root.join 'app', 'assets', 'components'
+
+          redirects = ::Rails.application.assets.paths.map do |p|
+            "\"#{default_asset_path}|#{File.join p, 'components'}\""
           end
 
           "--redirect #{redirects.join ' --redirect'}"
